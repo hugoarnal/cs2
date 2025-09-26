a6c49f019c82e5cf4f5e0fc3b5a5c39c0d82aea0
Hugo ARNAL <hugo@hugoarnal.com>
2025-09-26 16:42:00 +0200

    imp: inform more about compilation & installation

--- compile.sh
+++ compile.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+## This script is used to compile when installing or updating cs2.
+
+INSTALL_PATH=/usr/local/bin
 
 cargo build --release
-sudo cp -f $(dirname "$0")/target/release/cs2 /usr/local/bin/cs2
+
+if sudo cp -f $(dirname "$0")/target/release/cs2 $INSTALL_PATH/cs2; then
+    echo "Successfully compiled cs2!"
+    if [[ $PATH != *"$INSTALL_PATH"* ]]; then
+        echo "$INSTALL_PATH is not in your PATH environnement variable."
+        echo "Make sure to add it before you can use cs2."
+    fi
+else
+    echo "An error occurred, couldn't compile cs2."
+fi
