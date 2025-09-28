48561e8e296158dd65377c98c3584f419df56c1c
Hugo ARNAL <hugo@hugoarnal.com>
2025-09-28 14:42:03 +0200

    imp: cleaner `from_output` & error handling

--- src/build_systems.rs
+++ src/build_systems.rs
@@ -4,26 +4,21 @@ use std::process::Command;
 
 fn from_output(output: Vec<u8>) -> Result<Vec<String>, Error> {
     // TODO: replace unwrap if possible
-    let stdout_string = String::from_utf8(output).unwrap();
-    let str_vec: Vec<&str> = stdout_string.split("\n").collect();
-    // TODO: replace all this please
-    let mut lines = Vec::new();
+    let output_str = String::from_utf8(output).unwrap();
 
-    for line in str_vec {
-        println!("line: {}", line);
-        lines.push(String::from(line));
-    }
-
-    Ok(lines)
+    Ok(output_str
+        .split("\n")
+        .map(|f| String::from(f))
+        .collect::<Vec<_>>())
 }
 
 // TODO: use enums
 
 pub fn find() -> Result<Vec<String>, Error> {
-    let paths = fs::read_dir("./").unwrap();
+    let paths = fs::read_dir("./")?;
 
     for path in paths {
-        let file_name = path.unwrap().file_name().to_ascii_lowercase();
+        let file_name = path?.file_name().to_ascii_lowercase();
         if file_name == "makefile" || file_name == "gnumakefile" {
             break;
         }
