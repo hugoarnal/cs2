a8928f86b7cd8693be69fc116ae8c8f08a3b67e3
Hugo ARNAL <hugo@hugoarnal.com>
2025-10-04 17:37:40 +0200

    fix: check clang version on debian

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -47,9 +47,13 @@ fn move_to_final_path(temp_path: &str, final_path: &Path) -> Result<(), Error> {
 /// if clang-20 doesn't exist, check that clang installed version is `> 20`
 /// if it is, create symlink for clang-20 in `/usr/local/bin`
 fn verify_clang_version() -> Result<(), Error> {
-    if Path::new("/usr/bin/clang-20").exists() {
-        return Ok(());
-    };
+    let possible_paths = ["/usr/bin", "/usr/local/bin"];
+
+    for path in possible_paths {
+        if Path::new(&format!("{}/clang-20", path)).exists() {
+            return Ok(());
+        };
+    }
 
     if !Path::new("/usr/bin/clang").exists() {
         return Err(Error::other("Impossible to find clang, is it installed?"));
@@ -62,23 +66,29 @@ fn verify_clang_version() -> Result<(), Error> {
 
     let version_string = match String::from_utf8(version_output.stdout)
         .unwrap()
-        .split(" ")
-        .nth(2)
+        .split("version ")
+        .nth(1)
     {
         Some(v) => v.to_string(),
         None => return Err(Error::other("Impossible to get clang version")),
     };
 
-    let major: i32 = version_string.split(".").next().unwrap().parse().unwrap();
-    if major > 20 {
+    let major: i32 = match version_string.split(".").next() {
+        Some(s) => s.parse().unwrap(),
+        None => return Err(Error::other("Impossible to get the clang major version")),
+    };
+
+    if major >= 20 {
         let _ = Command::new("sudo")
             .args(["ln", "-s", "/usr/bin/clang", "/usr/local/bin/clang-20"])
             .spawn()?
             .wait();
         shared::warn_path_var("/usr/local/bin");
+
+        return Ok(());
     }
 
-    return Err(Error::other("clang version is not >= 20"));
+    Err(Error::other("clang version is not >= 20"))
 }
 
 fn epiclang() -> Result<(), Error> {
