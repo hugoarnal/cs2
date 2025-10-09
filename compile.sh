c5d4b14598a454fc2b50fb6cf84960e45264e64e
Hugo ARNAL <hugo@hugoarnal.com>
2025-10-10 01:23:54 +0200

    fix: permission denied & typo in compile.sh

--- compile.sh
+++ compile.sh
@@ -8,7 +8,8 @@ BASE_DIR=$(dirname "$0")
 
 ## needed for patches
 if [[ ! -d "/usr/local/share/cs2/cs2" ]]; then
-    cp -r $BASE_DIR /usr/local/share/cs2/cs2
+    sudo mkdir -p /usr/local/share/cs2
+    sudo cp -r $BASE_DIR /usr/local/share/cs2/cs2
 fi
 
 INSTALL_PATH=/usr/local/bin
@@ -22,7 +23,7 @@ if sudo cp -f $BASE_DIR/target/release/cs2 $INSTALL_PATH/cs2; then
         echo "$INSTALL_PATH is not in your PATH environnement variable."
         echo "Make sure to add it before you can use cs2."
     fi
-    echo "You can now install epiclang or banana with \"cs2 update\""
+    echo "You can now install epiclang or banana with \"cs2 install\""
 else
     echo "An error occurred, couldn't compile cs2."
 fi
