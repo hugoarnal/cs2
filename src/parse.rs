27a057da4e6ea731a922d8ac4c3e008a620caa54
Hugo ARNAL <hugo@hugoarnal.com>
2026-06-27 00:39:58 +0200

    fix: use `sort_by_key` instead of `sort_by`
    Clippy suggests this in newer versions

--- src/parse.rs
+++ src/parse.rs
@@ -302,9 +302,9 @@ fn my_dedup(errors: &mut Vec<LineError>) {
 
 /// remove duplicates by checking with PartialEq (dedup)
 fn clean_errors_vector(errors: &mut Vec<LineError>) {
-    errors.sort_by(|a, b| a.line_nb.cmp(&b.line_nb));
-    errors.sort_by(|a, b| a.col_nb.cmp(&b.col_nb));
-    errors.sort_by(|a, b| a.file.to_lowercase().cmp(&b.file.to_lowercase()));
+    errors.sort_by_key(|a| a.line_nb);
+    errors.sort_by_key(|a| a.col_nb);
+    errors.sort_by_key(|a| a.file.to_lowercase());
 
     my_dedup(errors);
 }
